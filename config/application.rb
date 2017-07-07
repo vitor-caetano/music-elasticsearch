266f810f1db52cd8bd66702abd15b515ffb2f9c9
Vitor Caetano <vitor.e.caetano@gmail.com>
2017-07-07 13:04:08 -0300

    Added autoloading of the ./lib folder

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ Bundler.require(*Rails.groups)
 
 module Music
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/lib)
+
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
